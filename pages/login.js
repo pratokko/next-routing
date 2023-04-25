561b014db1590ed984458c6d49e759a61f71b1f4
pratokko <pratokko1@gmail.com>
2023-04-25 22:55:06 +0300

    first commit

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import Navbar from "@/app/components/NavBar"
+
+
+const login = () => {
+  return (
+    <div>
+        <Navbar />
+        <h2> this is the login</h2>
+    </div>
+  )
+}
+export default login
